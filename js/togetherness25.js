d5439b8a475ecc48b0886e51bd36388ae4a986f1
Joshua Bousel <josh@bousel.com>
2025-03-24 17:06:44 -0400

    Added updating RSVP functionality

--- js/togetherness25.js
+++ js/togetherness25.js
@@ -63,8 +63,8 @@ $(function() {
 	
 	$('body').on('click','.rsvp__container__quanity',function(e){
 		e.preventDefault();
-		let adults = $('select[name="adults"').find(":selected").val();
-		let children = $('select[name="children"').find(":selected").val();
+		let adults = $('select[name="adults"]').find(":selected").val();
+		let children = $('select[name="children"]').find(":selected").val();
 		let data = { action: 'save', adults: adults, children: children };
 		updateRSVP(data);
 	});
@@ -89,6 +89,67 @@ $(function() {
 		let data = { action: 'save', note: note };
 		updateRSVP(data);
 	});
+	
+	$('body').on('click','.rsvp__container__edit',function(e){
+		e.preventDefault();
+		$.ajax({
+			async: true,
+			method: "POST",
+			url: "/php/rsvp-actions.php",
+			data: { action: 'edit' }
+		})
+		.done(function(content) {
+			$('.rsvp__container').html(content);
+			setRSVP();
+		});
+	});
+	
+	$('body').on('click','.rsvp__container__rsvp-cancel',function(e){
+		e.preventDefault();
+		let data = { action: 'cancel' };
+		updateRSVP(data);
+	});
+	
+	$('body').on('change','input[name="rsvp"]',function(e){
+		setRSVP();
+	});
+	
+	$('body').on('change','input[name="transportation"]',function(e){
+		setRSVP();
+	});
+	
+	$('body').on('click','.rsvp__container__rsvp-save',function(e){
+		e.preventDefault();
+		let rsvp = $('input[name="rsvp"]:checked').val();
+		let adults = $('select[name="adults"]').find(":selected").val();
+		let children = $('select[name="children"]').find(":selected").val();
+		let transportation = $('input[name="transportation"]:checked').val();
+		let carpool = $('input[name="carpool"]:checked').val();
+		let note = $('textarea[name="note"]').val();
+		let data = { action: 'update', rsvp: rsvp, adults: adults, children: children, transportation: transportation, carpool: carpool, note: note };
+		updateRSVP(data);
+	});
+	
+	function setRSVP() {
+		let rsvp = $('input[name="rsvp"]:checked').val();
+		let transportation = $('input[name="transportation"]:checked').val();
+
+		if (rsvp == "No") {
+			$('fieldset[name="quantity"],fieldset[name="transportation"],fieldset[name="carpool"]').addClass('hidden');
+		} else {
+			$('fieldset[name="quantity"],fieldset[name="transportation"]').removeClass('hidden');
+			
+			if (transportation == "Bus") {
+				$('fieldset[name="carpool"]').addClass('hidden');
+			} else if (transportation == "Car") {
+				$('fieldset[name="carpool"]').removeClass('hidden');
+			}
+			
+			if (carpool != "") {
+				$('fieldset[name="carpool"]').removeClass('hidden');
+			}
+		}
+	}
 	
 	function updateRSVP(data) {
 		$.ajax({
--- php/rsvp-actions.php
+++ php/rsvp-actions.php
@@ -1,7 +1,9 @@
 <?php
+$action = "";
+
 if (isset($_POST['action'])) {
 	include_once('togetherness25.php');
-	
+		
 	$guest = "";
 	if (isset($_COOKIE["guest"])) {
 		$guest = $_COOKIE["guest"];
@@ -35,8 +37,38 @@ if (isset($_POST['action'])) {
 		
 		$update = "UPDATE guests SET $set WHERE uuid = '$guest'";
 		dbQuery($update);	
+	} elseif ($_POST['action'] == "edit") {
+		$action = "edit";
+	} elseif ($_POST['action'] == "update") {
+		$rsvp = $_POST['rsvp'];
+		$adults = $_POST['adults'];
+		$children = $_POST['children'];
+		$transportation = $_POST['transportation'];
+		$carpool = $_POST['carpool'];
+		$note = $_POST['note'];
+		
+		if ($rsvp == "No") {
+			$adults = null;
+			$children = null;
+			$transportation = null;
+			$carpool = null;
+		} else {
+			if ($transportation == "Bus") {
+				$carpool = "N/A";
+			} elseif ($carpool == "") {
+				$carpool = "N/A";
+			}	
+		}
+		
+		if ($note == "") {
+			$note = "None";
+		} else {
+			$note = addslashes($note);
+		}
+		
+		$update = "UPDATE guests SET rsvp = '$rsvp', adults = '$adults', children = '$children', transportation = '$transportation', carpool = '$carpool', note = '$note' WHERE uuid = '$guest'";
+		dbQuery($update);
 	}
-	
 }
 
 $rsvpSelect = "SELECT first_name, last_name, rsvp, adults, children, transportation, carpool, note FROM guests WHERE uuid = '$guest'";
@@ -48,63 +80,115 @@ $adults = $row[3];
 $children = $row[4];
 $transportation = $row[5];
 $carpool = $row[6];
-$note = $row[7]; ?>
+$note = $row[7];
 
-<h2>Hi <?php echo($name); ?></h2>
+if ($note == "None") {
+	$note = "";
+} elseif ($note != "") {
+	$note = stripslashes($note);
+} ?>
 
-<?php if ($rsvp == "") { ?>
-<h3>Will you be attending?</h3>
-<a href="#" class="rsvp__container__attending" data-rsvp="Yes">Yes</a>
-<a href="#" class="rsvp__container__attending" data-rsvp="No">No</a>
-<?php } else { 
-	if ($rsvp == "Yes") {
-		if ($adults == "") { ?>
-			<h3>How many in your group?</h3>
-			<p><strong>Adults:</strong></p>
-			<select name="adults">
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
-			</select>
-			<p><strong>Children:</strong></p>
-			<select name="children">
-				<option value="0">0</option>
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-				<option value="4">4</option>
-				<option value="5">5</option>
-			</select>	
-			<a href="#" class="rsvp__container__quanity">Next</a>
-		<?php } else {
-			if ($transportation == "") { ?>
-				<h3>How do plan to travel to the party?</h3>
-				<a href="#" class="rsvp__container__transportation" data-transportation="Bus">Take the bus</a>
-				<a href="#" class="rsvp__container__transportation" data-transportation="Car">Drive/Rideshare</a>
+<?php if ($action == "edit") { ?>
+	<h2>Edit RSVP</h2>
+	<fieldset>
+		<legend>Will you be attending?</legend>
+		<input type="radio" id="rsvp-yes" name="rsvp" value="Yes"<?php if ($rsvp == "Yes"){ echo(" checked"); }?>> <label for="rsvp-yes">Yes</label>
+		<input type="radio" id="rsvp-no" name="rsvp" value="No"<?php if ($rsvp == "No"){ echo(" checked"); }?>> <label for="rsvp-no">No</label>
+	</fieldset>
+	<fieldset name="quantity">
+		<legend>How many in your group?</legend>
+		<label for="adults">Adults:</label>
+		<select id="adults" name="adults">
+			<?php for ($x = 1; $x <= 5; $x++) { ?>
+				<option value="<?php echo($x); ?>"<?php if ($adults == $x){ echo(" selected"); }?>><?php echo($x); ?></option>
+			<?php } ?>
+		</select>
+		<label for="children">Children:</label>
+		<select id="children" name="children">
+			<?php for ($x = 0; $x <= 5; $x++) { ?>
+				<option value="<?php echo($x); ?>"<?php if ($children == $x){ echo(" selected"); }?>><?php echo($x); ?></option>
+			<?php } ?>
+		</select>
+	</fieldset>
+	<fieldset name="transportation">
+		<legend>How do plan to travel to the party?</legend>
+		<input type="radio" id="bus" name="transportation" value="Bus"<?php if ($transportation == "Bus"){ echo(" checked"); }?>> <label for="bus">Take the bus</label>
+		<input type="radio" id="car" name="transportation" value="Car"<?php if ($transportation == "Car"){ echo(" checked"); }?>> <label for="car">Drive/Rideshare</label>
+	</fieldset>
+	<fieldset name="carpool" class="hidden">
+		<legend>If you plan on driving, would you be willing to carpool?</legend>
+		<input type="radio" id="carpool-yes" name="carpool" value="Yes"<?php if ($carpool == "Yes"){ echo(" checked"); }?>> <label for="carpool-yes">Yes</label>
+		<input type="radio" id="carpool-no" name="carpool" value="No"<?php if ($carpool == "No"){ echo(" checked"); }?>> <label for="carpool-no">No</label>
+		<input type="radio" id="carpool-na" name="carpool" value="N/A"<?php if ($carpool == "N/A"){ echo(" checked"); }?>> <label for="carpool-na">N/A</label>
+	</fieldset>
+	<fieldset name="note">
+		<legend>Leave a public note if you feel like it</legend>
+		<textarea name="note"><?php echo($note); ?></textarea>
+	</fieldset>
+	<p><a href="#" class="rsvp__container__rsvp-save">Submit</a> <a href="#" class="rsvp__container__rsvp-cancel">Cancel</a></p>
+<?php } else {
+	if ($rsvp == "") { ?>
+	<h2>Hi <?php echo($name); ?></h2>
+	<h3>Will you be attending?</h3>
+	<a href="#" class="rsvp__container__attending" data-rsvp="Yes">Yes</a>
+	<a href="#" class="rsvp__container__attending" data-rsvp="No">No</a>
+	<?php } else { 
+		if ($rsvp == "Yes") {
+			if ($adults == "") { ?>
+				<h3>How many in your group?</h3>
+				<p><strong>Adults:</strong></p>
+				<select name="adults">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select>
+				<p><strong>Children:</strong></p>
+				<select name="children">
+					<option value="0">0</option>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select>	
+				<a href="#" class="rsvp__container__quanity">Next</a>
 			<?php } else {
-				if ($transportation == "Car" && $carpool == "") { ?>
-					<h3>If you plan on driving, would you be willing to carpool?</h3>
-					<a href="#" class="rsvp__container__carpool" data-carpool="Yes">Yes</a>
-					<a href="#" class="rsvp__container__carpool" data-carpool="No">No</a>
-					<a href="#" class="rsvp__container__carpool" data-carpool="N/A">N/A</a>
-				<?php } else { 
-					if ($note == "") { ?>
-						<h3>Leave a public note if you feel like it</h3>
-						<textarea name="note"></textarea><br>
-						<a href="#" class="rsvp__container_note">Submit</a>
-						<a href="#" class="rsvp__container_note">No Thanks</a>
-					<?php } else { ?>
-						<h3>Thanks for your RSVP <?php echo($name); ?></h3>
-						<p>We look forward to seeing you in October!</p>
-						<p><a href="#" class="rsvp__container_edit">Change your RSVP</a></p>
-					<?php }
+				if ($transportation == "") { ?>
+					<h3>How do plan to travel to the party?</h3>
+					<a href="#" class="rsvp__container__transportation" data-transportation="Bus">Take the bus</a>
+					<a href="#" class="rsvp__container__transportation" data-transportation="Car">Drive/Rideshare</a>
+				<?php } else {
+					if ($transportation == "Car" && $carpool == "") { ?>
+						<h3>If you plan on driving, would you be willing to carpool?</h3>
+						<a href="#" class="rsvp__container__carpool" data-carpool="Yes">Yes</a>
+						<a href="#" class="rsvp__container__carpool" data-carpool="No">No</a>
+						<a href="#" class="rsvp__container__carpool" data-carpool="N/A">N/A</a>
+					<?php } else { 
+						if ($note == "") { ?>
+							<h3>Leave a public note if you feel like it</h3>
+							<textarea name="note"></textarea><br>
+							<a href="#" class="rsvp__container_note">Submit</a>
+							<a href="#" class="rsvp__container_note">No Thanks</a>
+						<?php } else { ?>
+							<h3>Thanks for your RSVP <?php echo($name); ?></h3>
+							<p>We look forward to seeing you in October!</p>
+							<p><a href="#" class="rsvp__container__edit">Change your RSVP</a></p>
+						<?php }
+					}
 				}
 			}
-		}
-	} else { ?>
-		<h3>Thanks for your RSVP <?php echo($name); ?></h3>
-		<p><a href="#" class="rsvp__container_edit">Change your RSVP</a></p>
-	<?php }
+		} else {
+			if ($note == "") { ?>
+				<h3>Leave a public note if you feel like it</h3>
+				<textarea name="note"></textarea><br>
+				<a href="#" class="rsvp__container_note">Submit</a>
+				<a href="#" class="rsvp__container_note">No Thanks</a>
+			<?php } else { ?>
+				<h3>Thanks for your RSVP <?php echo($name); ?></h3>
+				<p><a href="#" class="rsvp__container__edit">Change your RSVP</a></p>
+			<?php }
+		}	
+	}
 } ?>
